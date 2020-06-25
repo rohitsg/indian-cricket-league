895ca5e3ae93922d811ae43ed4662f889d9d9005
rohit goudagaon <rohitsgoudagaon@gmail.com>
2020-06-25 09:46:14 +0530

    combined all previous commits into 1 commit and updated commit message by using git rebase -i HEAD~6 and changing the pick to squash

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/', (req, res) => res.send('Post - Hello World!'))
+app.delete('/', (req, res) => res.send('Delete - Hello World!'))
+app.put('/', (req, res) => res.send('put - Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
